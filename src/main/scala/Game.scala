22e5b56f7348043133d6d21c26903e0f8b978416
Lishan Abbas <08lishan80@gmail.com>
2017-10-19 20:04:27 +0800

    Indexes Allocated for Enemies and Bullets
    - Enemies now disappear after hit
    - scene.content does not get overloaded with objects

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,7 +1,7 @@
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.scene.Scene
+import scalafx.scene.{Node, Scene}
 import scalafx.event.ActionEvent
 import scalafx.scene.text.Text
 import scalafx.scene.shape.Circle
@@ -16,7 +16,7 @@ class Game extends JFXApp {
 		title = "Game"
 		scene = new Scene(400, 600) {
 			var timerText = new Text(10, 20, "0.0")
-			var enemies = ArrayBuffer(Circle(10,10,10))
+			var enemies: ArrayBuffer[Circle] = ArrayBuffer()
 
 			var bullets: ArrayBuffer[Circle] = ArrayBuffer()
 			val player = new Circle() {
@@ -26,8 +26,20 @@ class Game extends JFXApp {
 				fill = Color.Blue
 			}
 
-			content = List(enemies.head, player)
-			content += timerText
+			content = List(player, timerText)
+
+			// Reserve space for bullets
+			var bulletsCounter = 2
+			for (i <- 0 until 20) {
+				content += new Circle()
+			}
+
+			// Reserve space for enemies
+			var enemiesCounter = 22
+			for (i <- 0 to 10) {
+				content += new Circle()
+			}
+
 
 			var keys = Map(
 				"Right" -> false,
@@ -45,7 +57,11 @@ class Game extends JFXApp {
 						fill = Color.Red
 					}
 
-					content += bullets.head
+					content(bulletsCounter) = bullets.head
+					bulletsCounter += 1
+					if (bulletsCounter > 21) {
+						bulletsCounter = 2
+					}
 				}
 			}
 
@@ -59,7 +75,7 @@ class Game extends JFXApp {
 			var enemySpeed = 80
 			val bulletSpeed = 300
 			val playerSpeed = 130
-			var spawnDelay = 2.0
+			var spawnDelay = 1.0
 			var seconds = 0.0
 
 			val timer: AnimationTimer = AnimationTimer(t => {
@@ -68,39 +84,43 @@ class Game extends JFXApp {
 
 					var indexes: ArrayBuffer[Int] = ArrayBuffer()
 
-					for (i <- 0 until enemies.length) {
-						val dx = player.centerX.value - enemies(i).centerX.value
-						val dy = player.centerY.value - enemies(i).centerY.value
-
-						val dist = math.sqrt(dx*dx + dy*dy)
-
-						if (dist <= enemies(i).radius.value + player.radius.value) {
-							content += new Text(195, 300, "You Lose!") 
-							timer.stop
-						}
-
-						enemies(i).centerX = enemies(i).centerX.value + dx / dist * enemySpeed * delta
-						enemies(i).centerY = enemies(i).centerY.value + dy / dist * enemySpeed * delta
-
-						for (b <- bullets) {
-							val dx = b.centerX.value - enemies(i).centerX.value
-							val dy = b.centerY.value - enemies(i).centerY.value
+					// Enemies
+					if (!enemies.isEmpty) {
+						for (i <- 0 until enemies.length) {
+							val dx = player.centerX.value - enemies(i).centerX.value
+							val dy = player.centerY.value - enemies(i).centerY.value
 
 							val dist = math.sqrt(dx*dx + dy*dy)
 
-							if (dist <= enemies(i).radius.value + b.radius.value) {
-								if (!indexes.contains(i)) {
-									indexes += i
+							if (dist <= enemies(i).radius.value + player.radius.value) {
+								content += new Text(195, 300, "You Lose!") 
+								timer.stop
+							}
+
+							enemies(i).centerX = enemies(i).centerX.value + dx / dist * enemySpeed * delta
+							enemies(i).centerY = enemies(i).centerY.value + dy / dist * enemySpeed * delta
+
+							for (b <- bullets) {
+								val dx = b.centerX.value - enemies(i).centerX.value
+								val dy = b.centerY.value - enemies(i).centerY.value
+
+								val dist = math.sqrt(dx*dx + dy*dy)
+
+								if (dist <= enemies(i).radius.value + b.radius.value) {
+									enemies(i).centerY = -20
+									if (!indexes.contains(i)) {
+										indexes += i
+									}
 								}
 							}
 						}
-					}
-					for (i <- 0 until indexes.length) {
-						enemies.remove(indexes(i))
-						enemiesKilled += 1
+						for (i <- 0 until indexes.length) {
+							enemies.remove(indexes(i))
+							enemiesKilled += 1
+						}
 					}
 
-
+					// Bullets
 					if (!bullets.isEmpty) {
 						indexes = ArrayBuffer()
 						for (i <- 0 until bullets.length) {
@@ -114,6 +134,7 @@ class Game extends JFXApp {
 						}
 					}
 
+					// Player
 					if (keys("Right") && (player.centerX.value + player.radius.value < 400)) {
 						player.centerX = player.centerX.value + playerSpeed*delta
 					}
@@ -122,15 +143,30 @@ class Game extends JFXApp {
 						player.centerX = player.centerX.value - playerSpeed*delta
 					}
 
+					// Enemies Spawn
 					spawnDelay -= delta
 					seconds += delta
 					if (spawnDelay < 0) {
 						val e = Circle(math.random * 400, 0, 10)
-						content += e
+						content(enemiesCounter) = e
+						enemiesCounter += 1
+
+						if (enemiesCounter > 32) {
+							enemiesCounter = 22
+						}
+
 						enemies +:= e
-						spawnDelay = 5.0
+						spawnDelay = 1.0
 					}
-					println(content)
+
+					// DEBUG
+					// println(content.length)
+					// var count = 0
+					// for (i <- content) {
+					// 	println(s"$count ${i.toString}")
+					// 	count += 1
+					// }
+					// println()
 					timerText.text = "%.1f".format(seconds)
 				}
 				lastTime = t
